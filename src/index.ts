04efc5a75893beda8f8d69077813be7f29771484
Artem1yMarenkov <orvysegor@gmail.com>
2022-12-05 23:35:27 +0300

    add .github/worklow dir and fix .husky/pre-commit

--- src/index.ts
+++ src/index.ts
@@ -7,68 +7,68 @@ import { IncomingMessage } from "webpack-dev-server";
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
 
 interface IReqBody extends RequestGenericInterface {
-    Body: {
-        link: string;
-        cookie: string;
-    };
+	Body: {
+		link: string;
+		cookie: string;
+	};
 }
 
 const app = Fastify({
-    logger: true,
-    https: {
-        cert: fs.readFileSync("cert.pem"),
-        key: fs.readFileSync("key.pem"),
-    },
+	logger: true,
+	https: {
+		cert: fs.readFileSync("cert.pem"),
+		key: fs.readFileSync("key.pem"),
+	},
 });
 
 app.post<IReqBody>("/table", async (req, res) => {
-    const { link, cookie } = req.body;
-    const url = new URL(link);
+	const { link, cookie } = req.body;
+	const url = new URL(link);
 
-    const fileName = `${cookie}-${new Date()}.xls`;
-    const file = fs.createWriteStream(fileName);
+	const fileName = `${cookie}-${new Date()}.xls`;
+	const file = fs.createWriteStream(fileName);
 
-    const response = await new Promise<IncomingMessage & FollowResponse>(
-        (resolve, reject) => {
-            https.get(
-                {
-                    hostname: url.hostname,
-                    pathname: url.pathname,
-                    headers: {
-                        cookie: req.headers["cookie"],
-                        "User-Agent": req.headers["user-agent"],
-                    },
-                },
-                (response) => {
-                    resolve(response);
-                }
-            );
-        }
-    );
+	const response = await new Promise<IncomingMessage & FollowResponse>(
+		(resolve, reject) => {
+			https.get(
+				{
+					hostname: url.hostname,
+					pathname: url.pathname,
+					headers: {
+						cookie: req.headers["cookie"],
+						"User-Agent": req.headers["user-agent"],
+					},
+				},
+				(response) => {
+					resolve(response);
+				}
+			);
+		}
+	);
 
-    response.pipe(file);
-    file.close();
+	response.pipe(file);
+	file.close();
 
-    const xlsFile = xlsx.read(fs.readFileSync(fileName));
-    const worksheet = xlsFile.Sheets["Выписка оценок"];
+	const xlsFile = xlsx.read(fs.readFileSync(fileName));
+	const worksheet = xlsFile.Sheets["Выписка оценок"];
 
-    const html = xlsx.utils.sheet_to_html(worksheet);
+	const html = xlsx.utils.sheet_to_html(worksheet);
 
-    return {
-        status: 200,
-        html: html,
-    };
+	return {
+		status: 200,
+		html: html,
+	};
 });
 
 const bootstrap = async () => {
-    try {
-        const port = 3000;
-        app.listen({ port });
-        console.info(`Server is running on http://localhost:${port}`);
-    } catch (error) {
-        app.log.error(error);
-        process.exit(1);
-    }
+	try {
+		const port = 3000;
+		app.listen({ port });
+		console.info(`Server is running on http://localhost:${port}`);
+	} catch (error) {
+		app.log.error(error);
+		process.exit(1);
+	}
 };
 
 bootstrap();
